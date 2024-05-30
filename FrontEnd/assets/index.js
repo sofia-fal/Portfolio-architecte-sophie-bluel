5a689917ca4af4b8831cf846129efff74d347e85
sofia-fal <sofia-fal@hotmail.com>
2024-05-30 13:40:56 +0200

    Affichage des travaux dans le DOM, ajout de fonction forEach

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,5 +1,22 @@
+const galleryGrid = document.querySelector(".gallery");
+
 async function getWorks() {
     const response = await fetch("http://localhost:5678/api/works");
     return await response.json();
 }
-getWorks();
+
+async function displayWorks() {
+    const arrayWorks = await getWorks();
+    arrayWorks.forEach(element => {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
+        img.src = element.imageUrl;
+        figcaption.textContent = element.title;
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        galleryGrid.appendChild(figure);
+    });
+}
+
+displayWorks();
