925ea4adbd06ffceb4534db47d791d0470482700
sofia-fal <sofia-fal@hotmail.com>
2024-06-03 15:48:13 +0200

    Tri des projets grâce à l'ajout des catégories et de la méthode filter

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,22 +1,28 @@
 const galleryGrid = document.querySelector(".gallery");
 const filterGrid = document.querySelector(".filters");
 
+//* Get and display works *//
+
 async function getWorks() {
     const works = await fetch("http://localhost:5678/api/works");
     return await works.json();
 }
 
+function createWorks(work) {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+    img.src = work.imageUrl;
+    figcaption.textContent = work.title;
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    galleryGrid.appendChild(figure);
+}
+
 async function displayWorks() {
     const arrayWorks = await getWorks();
-    arrayWorks.forEach(element => {
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const figcaption = document.createElement("figcaption");
-        img.src = element.imageUrl;
-        figcaption.textContent = element.title;
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        galleryGrid.appendChild(figure);
+    arrayWorks.forEach((work) => {
+        createWorks(work);
     });
 }
 
@@ -41,3 +47,28 @@ async function displayCategoriesBtn() {
 }
 
 displayCategoriesBtn();
+
+//* Category filters *//
+
+async function filterCategories() {
+    const allFilters = await getWorks();
+    const buttons = document.querySelectorAll(".filters button");
+    buttons.forEach(button => {
+        button.addEventListener("click", (e)=>{
+            btnId = e.target.id;
+            galleryGrid.innerHTML = "";
+            if (btnId !== "0") {
+                const filtering = allFilters.filter((works) => {
+                    return works.categoryId == btnId;
+                });
+                filtering.forEach(work => {
+                    createWorks(work);                  
+                }); 
+            } else {
+                displayWorks();
+            }
+        })
+    });
+}
+
+filterCategories();
