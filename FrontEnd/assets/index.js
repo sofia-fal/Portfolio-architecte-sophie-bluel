be5a117454bfa20810464c8eb0a6f58a26b7c1f6
sofia-fal <sofia-fal@hotmail.com>
2024-06-06 15:30:28 +0200

    Récupération du formulaire, quelques modifications

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -4,26 +4,26 @@ const filterGrid = document.querySelector(".filters");
 //* Get and display works *//
 
 async function getWorks() {
-    const works = await fetch("http://localhost:5678/api/works");
-    return await works.json();
+  const works = await fetch("http://localhost:5678/api/works");
+  return await works.json();
 }
 
 function createWorks(work) {
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const figcaption = document.createElement("figcaption");
-    img.src = work.imageUrl;
-    figcaption.textContent = work.title;
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    galleryGrid.appendChild(figure);
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
+  img.src = work.imageUrl;
+  figcaption.textContent = work.title;
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+  galleryGrid.appendChild(figure);
 }
 
 async function displayWorks() {
-    const arrayWorks = await getWorks();
-    arrayWorks.forEach((work) => {
-        createWorks(work);
-    });
+  const arrayWorks = await getWorks();
+  arrayWorks.forEach((work) => {
+    createWorks(work);
+  });
 }
 
 displayWorks();
@@ -31,19 +31,19 @@ displayWorks();
 //* Categories *//
 
 async function getCategories() {
-    const categories = await fetch("http://localhost:5678/api/categories");
-    return await categories.json();
+  const categories = await fetch("http://localhost:5678/api/categories");
+  return await categories.json();
 }
 
 async function displayCategoriesBtn() {
-    const categories = await getCategories();
+  const categories = await getCategories();
 
-    categories.forEach(category => {
-        const btn = document.createElement("button");
-        btn.textContent = category.name;
-        btn.id = category.id;
-        filterGrid.appendChild(btn);
-    });
+  categories.forEach((category) => {
+    const btn = document.createElement("button");
+    btn.textContent = category.name;
+    btn.id = category.id;
+    filterGrid.appendChild(btn);
+  });
 }
 
 displayCategoriesBtn();
@@ -51,24 +51,24 @@ displayCategoriesBtn();
 //* Category filters *//
 
 async function filterCategories() {
-    const allFilters = await getWorks();
-    const buttons = document.querySelectorAll(".filters button");
-    buttons.forEach(button => {
-        button.addEventListener("click", (e)=>{
-            btnId = e.target.id;
-            galleryGrid.innerHTML = "";
-            if (btnId !== "0") {
-                const filtering = allFilters.filter((works) => {
-                    return works.categoryId == btnId;
-                });
-                filtering.forEach(work => {
-                    createWorks(work);                  
-                }); 
-            } else {
-                displayWorks();
-            }
-        })
+  const allFilters = await getWorks();
+  const buttons = document.querySelectorAll(".filters button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      btnId = e.target.id;
+      galleryGrid.innerHTML = "";
+      if (btnId !== "0") {
+        const filtering = allFilters.filter((works) => {
+          return works.categoryId == btnId;
+        });
+        filtering.forEach((work) => {
+          createWorks(work);
+        });
+      } else {
+        displayWorks();
+      }
     });
+  });
 }
 
 filterCategories();
---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,3 @@
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
